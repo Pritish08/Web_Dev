06c174920d752f705f08ca3ea22b644a79bc56e3
Pritish08 <pritishsontakke08@gmail.com>
2026-06-25 16:36:04 +0530

    Modified

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-// console.log("Hello from new.html");
-        // // variables
-        // 1
-        // // var name = "John Doe";
-        // // var is either function scoped or globally scoped depending on where it is declared.
-        // function print(){
-        //     var name = "John Doe";
-        //     console.log(name);
-        // }
-        // print();
-        // console.log(name); // ReferenceError: name is not defined
+console.log("Hello from new.html");
+        // variables
+        1
+        // var name = "John Doe";
+        // var is either function scoped or globally scoped depending on where it is declared.
+        function print(){
+            var name = "John Doe";
+            console.log(name);
+        }
+        print();
+        console.log(name); // ReferenceError: name is not defined
         // 2
         // let name = "John Doe";  
         // let is block scoped, meaning it is only accessible within the block it is defined in.
