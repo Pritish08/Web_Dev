d551e240f399dee8a66151e95a2b61a32ba640e9
Pritish08 <pritishsontakke08@gmail.com>
2026-06-26 15:16:29 +0530

    Learned about Operators

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
-console.log("Hello from new.html");
-        // variables
-        1
-        // var name = "John Doe";
-        // var is either function scoped or globally scoped depending on where it is declared.
-        // function print(){
-        //     var name = "John Doe";
-        //     console.log(name);
-        // }
-        // print();
-        // console.log(name); // ReferenceError: name is not defined
-        // 2
-        let name = "John Doe";  
-        console.log(name);
-        // let is block scoped, meaning it is only accessible within the block it is defined in.
+// // // console.log("Hello from new.html");
+// //         // variables
+// //         1
+// //         // var name = "John Doe";
+// //         // var is either function scoped or globally scoped depending on where it is declared.
+// //         // function print(){
+// //         //     var name = "John Doe";
+// //         //     console.log(name);
+// //         // }
+// //         // print();
+// //         // console.log(name); // ReferenceError: name is not defined
+// //         // 2
+// //         let name = "John Doe";  
+// //         console.log(name);
+// //         // let is block scoped, meaning it is only accessible within the block it is defined in.
+
+// // Operators
+// let a = 10;
+// let b = 5;
+// console.log(a + b); // Addition
+// console.log(a - b); // Subtraction
+// console.log(a * b); // Multiplication
+// console.log(a / b); // Division
+// console.log(a % b);
+
+// Ternary Operator
+let Age = 20;
+let Ans = (Age>10)?"CAN VOte":"Cannot Vote";
+console.log(Ans);
