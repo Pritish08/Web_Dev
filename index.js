051204f4274b467bae2ae1ac96afbf4f9119fb8e
Pritish08 <pritishsontakke08@gmail.com>
2026-06-27 14:32:03 +0530

    String

--- index.js
+++ index.js
@@ -1,29 +1,47 @@
-// // // console.log("Hello from new.html");
-// //         // variables
-// //         1
-// //         // var name = "John Doe";
-// //         // var is either function scoped or globally scoped depending on where it is declared.
-// //         // function print(){
-// //         //     var name = "John Doe";
-// //         //     console.log(name);
-// //         // }
-// //         // print();
-// //         // console.log(name); // ReferenceError: name is not defined
-// //         // 2
-// //         let name = "John Doe";  
-// //         console.log(name);
-// //         // let is block scoped, meaning it is only accessible within the block it is defined in.
+// // // // // // console.log("Hello from new.html");
+// // // // //         // variables
+// // // // //         1
+// // // // //         // var name = "John Doe";
+// // // // //         // var is either function scoped or globally scoped depending on where it is declared.
+// // // // //         // function print(){
+// // // // //         //     var name = "John Doe";
+// // // // //         //     console.log(name);
+// // // // //         // }
+// // // // //         // print();
+// // // // //         // console.log(name); // ReferenceError: name is not defined
+// // // // //         // 2
+// // // // //         let name = "John Doe";  
+// // // // //         console.log(name);
+// // // // //         // let is block scoped, meaning it is only accessible within the block it is defined in.
 
-// // Operators
-// let a = 10;
-// let b = 5;
-// console.log(a + b); // Addition
-// console.log(a - b); // Subtraction
-// console.log(a * b); // Multiplication
-// console.log(a / b); // Division
-// console.log(a % b);
+// // // // // Operators
+// // // // let a = 10;
+// // // // let b = 5;
+// // // // console.log(a + b); // Addition
+// // // // console.log(a - b); // Subtraction
+// // // // console.log(a * b); // Multiplication
+// // // // console.log(a / b); // Division
+// // // // console.log(a % b);
 
-// Ternary Operator
-let Age = 20;
-let Ans = (Age>10)?"CAN VOte":"Cannot Vote";
-console.log(Ans);
+// // // // Ternary Operator
+// // // let Age = 20;
+// // // let Ans = (Age>10)?"CAN VOte":"Cannot Vote";
+// // // console.log(Ans);
+
+// // // Loops 
+// // for(let i=1 ; i<=10 ; i++){
+// //     console.log(i*2);
+// // }
+
+// // String
+// let name = "John Doe";
+// console.log(name);
+// name = `My
+// name 
+// is 
+// John Doe`;
+// console.log(name);
+let a = "op1";
+let b = "op2";
+let ans = `${a} ${b}`;
+console.log(ans);
